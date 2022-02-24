4897b06805a9b98b07b73333ff1d705821ef8cb5
Thomas Skerbis <thomas.skerbis@mac.com>
2022-02-24 11:11:17 +0100

    Assets nur laden wenn geolocation addon nicht verfügbar.

--- boot.php
+++ boot.php
@@ -1,11 +1,12 @@
 <?php
 
 if (rex::isBackend() && rex::getUser()) {
-	rex_yform::addTemplatePath(rex_path::addon('yform_geo_osm', 'ytemplates'));
-
-	rex_view::addJsFile($this->getAssetsUrl('leaflet/leaflet.js'));
-	rex_view::addCssFile($this->getAssetsUrl('leaflet/leaflet.css'));
-
+    rex_yform::addTemplatePath(rex_path::addon('yform_geo_osm', 'ytemplates'));
+	// Assets nur laden wenn geolocation addon nicht verfügbar. 
+	if (!rex_addon::get('geolocation')->isAvailable()) {
+           rex_view::addJsFile($this->getAssetsUrl('leaflet/leaflet.js'));
+           rex_view::addCssFile($this->getAssetsUrl('leaflet/leaflet.css'));
+    }
     rex_view::addJsFile($this->getAssetsUrl('geo_osm.js'));
     rex_view::addCssFile($this->getAssetsUrl('geo_osm.css'));
 }
