42d89a0bb4319a08720c99f7591cd683aa87236a
Thomas Skerbis <thomas.skerbis@mac.com>
2024-12-27 21:23:11 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -5,6 +5,7 @@ if (rex::isBackend() && rex::getUser()) {
 	// Assets nur laden wenn geolocation addon nicht verfügbar. 
 	if (!rex_addon::get('geolocation')->isAvailable()) {
            rex_view::addJsFile($this->getAssetsUrl('leaflet/leaflet.js'));
+	   rex_view::addJsFile($this->getAssetsUrl('CoordPicker.js'));
            rex_view::addCssFile($this->getAssetsUrl('leaflet/leaflet.css'));
     }
     rex_view::addJsFile($this->getAssetsUrl('geo_osm.js'));
