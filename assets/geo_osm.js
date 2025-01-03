1ef4739248b26159e92012626a0688309ad43752
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-03 16:52:20 +0100

    Update geo_osm.js

--- assets/geo_osm.js
+++ assets/geo_osm.js
@@ -1,65 +1,53 @@
 var rex_geo_osm = function(addressfields, geofields, id, mapbox_token) {
     var current_lat = $(geofields.lat).val();
     var current_lng = $(geofields.lng).val();
-    var hasCoordinates = current_lat && current_lng;
     
     // Map initialization
-    var mapOptions = {
-        gestureHandling: true,
-        duration: 500
-    };
+    var map = initMap();
+    var marker;
 
-    if (hasCoordinates) {
-        mapOptions.center = [current_lat, current_lng];
-        mapOptions.zoom = 16;
-    } else {
-        mapOptions.center = [30, 0];
-        mapOptions.zoom = 2;
+    if (current_lat && current_lng) {
+        setMarker(current_lat, current_lng);
+        $('#rex-geo-overlay-'+id).hide();
     }
 
-    var map;
-    if (mapbox_token) {
-        var mapboxAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Imagery © <a href="http://mapbox.com">Mapbox</a>';
-        var streets = L.tileLayer('//api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapbox_token, 
-            {id: 'mapbox.streets', attribution: mapboxAttribution}),
-            streets_satellite = L.tileLayer('//api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapbox_token, 
-            {id: 'mapbox.streets-satellite', attribution: mapboxAttribution});
+    function initMap() {
+        var mapOptions = {
+            gestureHandling: true,
+            duration: 500,
+            center: [30, 0],
+            zoom: 2
+        };
 
-        mapOptions.layers = [streets, streets_satellite];
-        map = L.map('map-'+id, mapOptions);
+        if (mapbox_token) {
+            var mapboxAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Imagery © <a href="http://mapbox.com">Mapbox</a>';
+            var streets = L.tileLayer('//api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapbox_token, 
+                {id: 'mapbox.streets', attribution: mapboxAttribution}),
+                streets_satellite = L.tileLayer('//api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapbox_token, 
+                {id: 'mapbox.streets-satellite', attribution: mapboxAttribution});
 
-        L.control.layers({
-            "Map": streets,
-            "Satellite": streets_satellite
-        }).addTo(map);
-    } else {
+            mapOptions.layers = [streets, streets_satellite];
+            var map = L.map('map-'+id, mapOptions);
+
+            L.control.layers({
+                "Map": streets,
+                "Satellite": streets_satellite
+            }).addTo(map);
+            
+            return map;
+        } 
+
         var streets = L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         });
         mapOptions.layers = [streets];
-        map = L.map('map-'+id, mapOptions);
+        return L.map('map-'+id, mapOptions);
     }
 
-    // Marker handling
-    var marker;
-    if (hasCoordinates) {
-        marker = L.marker([current_lat, current_lng], {
-            draggable: true
-        }).addTo(map);
-        
-        marker.on('dragend', function(ev) {
-            var pos = ev.target.getLatLng();
-            updateCoordinates(pos.lat, pos.lng);
-        });
-        
-        $('#rex-geo-overlay-'+id).hide();
-    }
-
-    function updateCoordinates(lat, lng) {
-        $(geofields.lat).val(lat.toFixed(6));
-        $(geofields.lng).val(lng.toFixed(6));
-        
-        if (!marker) {
+    function setMarker(lat, lng) {
+        if (marker) {
+            marker.setLatLng([lat, lng]);
+        } else {
             marker = L.marker([lat, lng], {
                 draggable: true
             }).addTo(map);
@@ -68,36 +56,26 @@ var rex_geo_osm = function(addressfields, geofields, id, mapbox_token) {
                 var pos = ev.target.getLatLng();
                 updateCoordinates(pos.lat, pos.lng);
             });
-            
-            $('#rex-geo-overlay-'+id).hide();
-        } else {
-            marker.setLatLng([lat, lng]);
         }
+        map.setView([lat, lng], 16);
+    }
+
+    function updateCoordinates(lat, lng) {
+        $(geofields.lat).val(lat.toFixed(6));
+        $(geofields.lng).val(lng.toFixed(6));
+        setMarker(lat, lng);
+        $('#rex-geo-overlay-'+id).hide();
     }
 
     // Search functionality
-    var searchInput = $('#rex-geo-search-input-'+id);
-    var searchResults = $('#rex-geo-search-results-'+id);
     var searchTimeout;
-
-    // Pre-fill search only if we don't have coordinates
-    if (!hasCoordinates && addressfields.length > 0) {
-        var address = [];
-        addressfields.forEach(function(selector) {
-            var value = $(selector).val();
-            if (value) address.push(value);
-        });
-        if (address.length > 0) {
-            searchInput.val(address.join(', '));
-        }
-    }
-
-    searchInput.on('input focus', function() {
+    $('#rex-geo-search-input-'+id).on('input focus', function() {
         clearTimeout(searchTimeout);
         var value = $(this).val();
+        var $results = $('#rex-geo-search-results-'+id);
         
         if (value.length < 3) {
-            searchResults.removeClass('active').empty();
+            $results.removeClass('active').empty();
             return;
         }
 
@@ -108,10 +86,10 @@ var rex_geo_osm = function(addressfields, geofields, id, mapbox_token) {
                 limit: 5
             })
             .done(function(data) {
-                searchResults.empty();
+                $results.empty();
                 
                 if (data.length === 0) {
-                    searchResults.append(
+                    $results.append(
                         $('<div class="rex-geo-search-result">').text('No results found')
                     );
                 } else {
@@ -119,31 +97,22 @@ var rex_geo_osm = function(addressfields, geofields, id, mapbox_token) {
                         $('<div class="rex-geo-search-result">')
                             .text(result.display_name)
                             .on('click', function() {
-                                selectLocation(result);
-                                searchResults.removeClass('active');
+                                updateCoordinates(result.lat, result.lon);
+                                $(this).closest('.rex-geo-search-results').removeClass('active');
                             })
-                            .appendTo(searchResults);
+                            .appendTo($results);
                     });
                 }
-                searchResults.addClass('active');
+                $results.addClass('active');
             });
         }, 300);
     });
 
     $(document).on('click', function(e) {
         if (!$(e.target).closest('.rex-geo-search-wrapper').length) {
-            searchResults.removeClass('active');
+            $('.rex-geo-search-results').removeClass('active');
         }
     });
-
-    function selectLocation(location) {
-        var lat = parseFloat(location.lat);
-        var lng = parseFloat(location.lon);
-        
-        updateCoordinates(lat, lng);
-        map.setView([lat, lng], 16);
-        searchInput.val(location.display_name);
-    }
 
     // Browser geolocation
     $('#browser-geo-'+id).on('click', function() {
@@ -154,11 +123,10 @@ var rex_geo_osm = function(addressfields, geofields, id, mapbox_token) {
         
         navigator.geolocation.getCurrentPosition(
             function(position) {
-                var lat = position.coords.latitude;
-                var lng = position.coords.longitude;
-                
-                updateCoordinates(lat, lng);
-                map.setView([lat, lng], 16);
+                updateCoordinates(
+                    position.coords.latitude,
+                    position.coords.longitude
+                );
             },
             function(error) {
                 alert("Geolocation failed: " + error.message);
